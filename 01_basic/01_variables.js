4bda8ab13bb57abae7d29950f90c619f068153fa
vinitbahadure <vinitbahadure312@gmail.com>
2026-01-17 19:49:03 +0530

    all new activitity of variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 172123314
+let acoountEmail = "vinit@best.com"
+var accountPassword = "12345"
+accountCity = "Pune"
+let accountState;
+
+// accountId = 2 // not allowed
+
+//accountId = 555
+
+console.log(accountId);
+
+accountPassword = "5645645663"
+acoountEmail = "randi@yash.com"
+accountCity = "Amravati"
+
+console.table([accountId, accountPassword, accountCity, acoountEmail, accountState])
